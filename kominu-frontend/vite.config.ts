59285c6eec604fb41adbe84fb06bd9e9a9718eca
Alexandra <al3xandra92@hotmail.com>
2025-06-28 17:15:44 +0200

    🔧 Configure dependencies for styling and alias support

--- kominu-frontend/vite.config.ts
+++ kominu-frontend/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
-})
+ resolve: {
+  alias: {
+    '@': path.resolve(__dirname, './src'),
+    '@styles': path.resolve(__dirname, './src/styles'), // lägg till denna!
+  },
+},
+  css: {
+    preprocessorOptions: {
+      scss: {
+      },
+    },
+  },
+});
