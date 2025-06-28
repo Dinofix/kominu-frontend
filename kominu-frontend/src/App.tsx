2f743ab9d1e29647cece0e5705abd2950a3701f5
Alexandra <al3xandra92@hotmail.com>
2025-06-28 12:17:15 +0200

    🎨 Add global SCSS structure with variables, mixins and base styles

--- kominu-frontend/src/App.tsx
+++ kominu-frontend/src/App.tsx
@@ -1,33 +1,14 @@
-import { useState } from 'react'
+
 import './App.scss'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-    
-        </a>
-        <a href="https://react.dev" target="_blank">
-
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div>
+      <h1>Välkommen till Kominu!</h1>
+      <p>Denna applikation är under utveckling.</p>
+     </div>
     </>
   )
 }
-
-export default App
